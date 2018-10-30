242d0343a7f99d138e91a79886e2f9ace2d31899
Deep Mind Hive <44175636+DeepHiveMind@users.noreply.github.com>
2018-10-30 13:18:21 +0530

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -5,16 +5,13 @@ import numpy as np
 data_dim = 16
 timesteps = 8
 num_classes = 10
-batch_size = 32
 
-# Expected input batch shape: (batch_size, timesteps, data_dim)
-# Note that we have to provide the full batch_input_shape since the network is stateful.
-# the sample of index i in batch k is the follow-up for the sample i in batch k-1.
+# expected input data shape: (batch_size, timesteps, data_dim)
 model = Sequential()
-model.add(LSTM(32, return_sequences=True, stateful=True,
-               batch_input_shape=(batch_size, timesteps, data_dim)))
-model.add(LSTM(32, return_sequences=True, stateful=True))
-model.add(LSTM(32, stateful=True))
+model.add(LSTM(32, return_sequences=True,
+               input_shape=(timesteps, data_dim)))  # returns a sequence of vectors of dimension 32
+model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
+model.add(LSTM(32))  # return a single vector of dimension 32
 model.add(Dense(10, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
@@ -22,14 +19,13 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 # Generate dummy training data
-x_train = np.random.random((batch_size * 10, timesteps, data_dim))
-y_train = np.random.random((batch_size * 10, num_classes))
+x_train = np.random.random((1000, timesteps, data_dim))
+y_train = np.random.random((1000, num_classes))
 
 # Generate dummy validation data
-x_val = np.random.random((batch_size * 3, timesteps, data_dim))
-y_val = np.random.random((batch_size * 3, num_classes))
+x_val = np.random.random((100, timesteps, data_dim))
+y_val = np.random.random((100, num_classes))
 
 model.fit(x_train, y_train,
-          batch_size=batch_size, epochs=5, shuffle=False,
+          batch_size=64, epochs=5,
           validation_data=(x_val, y_val))
- 
